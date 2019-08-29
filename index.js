6a5271a0c6def62a18705e5a888d4ea3e7ba8ee3
Hurricane <setesolutions@gmail.com>
2019-08-28 21:53:49 -0300

    chore: Alteração Hello World 3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/teste', (req, res) => {
-  return res.json({ hello: 'world 2'})
+  return res.json({ hello: 'world 3'})
 });
 
 app.listen(3333);
